06c7073eb3dda528af2197ef035c70fee68cc176
Marcus Capulong <mnlcapulong.student@ua.edu.ph>
2026-02-24 11:09:56 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="layout-wrapper">
+      <h2 className="header-name">Juan Dela Cruz</h2>
+      
+      <div className="grid-container">
+        <div className="grid-item item-1">1</div>
+        <div className="grid-item item-2">2</div>
+        <div className="grid-item item-3">3</div>
+        <div className="grid-item item-4">4</div>
+        <div className="grid-item item-5">5</div>
+        <div className="grid-item item-6">6</div>
+        <div className="grid-item item-7">7</div>
+        <div className="grid-item item-8">8</div>
+      </div>
+    </div>
+  )
+}
+
+export default App
